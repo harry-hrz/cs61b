d3f7db800f646ad75d1a3bbdc4e143869cfef8d1
Harry Zhang <harry.zhang@duke.edu>
2023-07-11 16:16:47 +0800

    finish lab01

--- lab01/Collatz.java
+++ lab01/Collatz.java
@@ -1,12 +1,23 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Harry Zhang
  */
 public class Collatz {
 
     /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
+        /**
+         * if n is odd, return 3n + 1
+         * if n is even, return n/2
+         */
         // TODO: Fill in this method.
-        return 1;
+        int result = 0;
+        if (n % 2 == 0) {
+            result = n / 2;
+        }
+        else{
+            result = n * 3 + 1;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
