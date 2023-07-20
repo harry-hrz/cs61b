9720d9b82dfcab8cd652ff8ebf48ba72f6615c27
harry-hrz <haoruizhang.cs@gmail.com>
2023-07-20 22:19:31 +0800

    init MaxArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,5 +1,18 @@
 package deque;
 
+import java.util.Comparator;
+
 public class MaxArrayDeque<DType> extends ArrayDeque<DType>{
-    
+    public MaxArrayDeque(Comparator<DType> c) {
+        super();
+        
+    }
+
+    public DType max() {
+        return null;
+    }
+
+    public DType max(Comparator<DType> c) {
+        return null;
+    }
 }
